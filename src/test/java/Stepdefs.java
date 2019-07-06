09664de274a0bc8031b01232f1116c6419199d0b
João Paulo Miranda <jpmirandas@gmail.com>
2019-07-06 09:34:15 -0300

    my first commit

---
+++ src/test/java/Stepdefs.java
@@ -0,0 +1,26 @@
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import static org.junit.Assert.*;
+
+public class Stepdefs {
+    @Given("today is Sunday")
+    public void today_is_Sunday() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("aewww=================================");
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_Friday_yet() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("aewww=================================");
+
+    }
+
+    @Then("I should be told {string}")
+    public void i_should_be_told(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("aewww=================================");
+    }
+}
